bd4722d66ed1cda2f177fd06a70fb6efb60b6947
elacalle <elacalle@nosolosoftware.es>
2020-01-01 14:12:16 +0100

    Fixed code style
    Fixed code tabs and whitespaces

--- 02_deep_foundations_coercion_resolved.js
+++ 02_deep_foundations_coercion_resolved.js
@@ -1,40 +1,40 @@
 function isValidName(name) {
-	if (
-		typeof name == "string" &&
-		name.trim().length >= 3
-	) {
-		return true;
-	}
+  if (
+    typeof name == "string" &&
+    name.trim().length >= 3
+  ) {
+    return true;
+  }
 
-	return false;
+  return false;
 }
 
 function hoursAttended(attended,length) {
-	if (
-		typeof attended == "string" &&
-		attended.trim() != ""
-	) {
-		attended = Number(attended);
-	}
-	if (
-		typeof length == "string" &&
-		length.trim() != ""
-	) {
-		length = Number(length);
-	}
-	if (
-		typeof attended == "number" &&
-		typeof length == "number" &&
-		attended <= length &&
-		attended >= 0 &&
-		length >= 0 &&
-		Number.isInteger(attended) &&
-		Number.isInteger(length)
-	) {
-		return true;
-	}
+  if (
+    typeof attended == "string" &&
+    attended.trim() != ""
+  ) {
+    attended = Number(attended);
+  }
+  if (
+    typeof length == "string" &&
+    length.trim() != ""
+  ) {
+    length = Number(length);
+  }
+  if (
+    typeof attended == "number" &&
+    typeof length == "number" &&
+    attended <= length &&
+    attended >= 0 &&
+    length >= 0 &&
+    Number.isInteger(attended) &&
+    Number.isInteger(length)
+  ) {
+    return true;
+  }
 
-	return false;
+  return false;
 }
 
 
